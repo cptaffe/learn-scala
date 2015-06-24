3c63a74bd38a0e6eae4377c80d68e3559ddd1a18
Connor Taffe <cpaynetaffe@gmail.com>
2015-06-23 23:24:24 -0500

    update

--- learn.scala
+++ learn.scala
@@ -1,7 +1,16 @@
-#!/usr/bin/env scala
+#!/bin/sh
+	exec scala -savecompiled "$0" "$@"
+!#
 
 object HelloWorld {
+
+	def fact(i : BigInt) : BigInt = i match {
+		case x : BigInt if x == 0 => 1
+		case _ => i * fact(i - 1)
+	}
+
 	def main(args: Array[String]) {
-		println("Hello, world!")
+		args.foreach((x : String) =>
+			println("Factorial of "+x+" is "+fact(x.toInt)));
 	}
 }
